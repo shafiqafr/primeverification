5b36b55226a9ed7432d51a58aee7459567c263d3
shafiqafr <141959250+shafiqafr@users.noreply.github.com>
2025-09-04 10:21:30 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+export const dynamic = 'force-client';
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
